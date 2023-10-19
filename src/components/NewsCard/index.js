da1ba8ebf90024ed3cd52193c55e82e8e26e8240
gaby <jock1087@gmail.com>
2023-10-19 16:50:13 +0300

    moved styles to index.css

--- src/components/NewsCard/index.js
+++ src/components/NewsCard/index.js
@@ -8,17 +8,7 @@ function NewsCard(props) {
 
   return (
     <Card title={title} style={{ marginBottom: '16px' }}>
-      <Row
-        style={{
-          marginBottom: '16px',
-          overflow: 'hidden',
-          display: '-webkit-box',
-          webkitLineClamp: '3',
-          webkitBoxOrient: 'vertical',
-        }}
-      >
-        {body}
-      </Row>
+      <Row className='newsText'>{body}</Row>
       <Row justify='space-between'>
         <Col>{tags && tags.map((tag) => <Tag key={tag}>{tag}</Tag>)}</Col>
         <Col align='middle'>
